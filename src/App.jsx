7f45bc17b1549d5b6ea52605db337574ca1f6931
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-08 17:06:28 +0100

    feat: form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import AppCard from './components/AppCard';
+import AppForm from './components/AppForm';
 
 
 
@@ -96,27 +97,11 @@ function App() {
   return (
     <>
       {/* Form posts */}
-      <form onSubmit={handleNewPostSubmit}>
-        <div className='container m-5 d-flex flex-column gap-3'>
-          <h2>Inserisci un nuovo post</h2>
-
-          <label htmlFor="title">Titolo post</label>
-          <input className='form-control' name='title' type="text" id='title' value={formData.title} onChange={handleInputChange} />
-
-          <label htmlFor='content'>Contenuto post</label>
-          <textarea className='form-control' name='content' type="text-area" id='content' value={formData.content} onChange={handleInputChange} />
-
-          <label htmlFor='image'>Url immagine del post</label>
-          <input className='form-control' type="url" id='image' name='image' value={formData.image} onChange={handleInputChange} />
-
-          <div className="d-flex gap-3">
-            <button type='submit' className={`btn btn-success ${(!formData.title || !formData.content || !formData.image) && 'disabled'}`}>Salva</button>
-            <button type='clear' className='btn btn-danger'>Cancella</button>
-          </div>
-
-
-        </div>
-      </form>
+      <AppForm
+      formData={formData}
+      onSubmit={handleNewPostSubmit}
+      onInputChange={handleInputChange}
+      />
 
       {/* Lista posts */}
       <div className="container m-5 d-flex flex-column gap-3">
